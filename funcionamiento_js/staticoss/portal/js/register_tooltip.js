eae9310173220e5eae19e9a353c91e8a76c3bd5e
raul-1601 <raul240sx@hotmail.com>
2025-09-26 16:30:31 +0000

    borrado de archivos innecesario o redundantes

--- funcionamiento_js/staticoss/portal/js/register_tooltip.js
+++
@@ -1,7 +0,0 @@
-// inicializa todos los tooltips de la página
-document.addEventListener("DOMContentLoaded", function() {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
